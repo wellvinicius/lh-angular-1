2652c1cfee656427716727c992bb3383e50d70c3
Felipe Santos de Jesus <felipe.sjesus@sp.senai.br>
2022-02-15 21:00:36 -0300

    Subindo front do Webinar

--- src/app/vagas.service.ts
+++ src/app/vagas.service.ts
@@ -8,25 +8,25 @@ import { Vaga } from './models/Vagas.model';
 })
 export class VagasService {
 
-  private url = "http://localhost:3000/vagas";
+  private url = "http://localhost:8080/api";
 
   constructor(private _httpClient: HttpClient) {  }
 
   getVagas(): Observable<Vaga[]>{
-    return this._httpClient.get<Vaga[]>(this.url);
+    return this._httpClient.get<Vaga[]>(this.url+"/vagas");
   }
 
   cadastrarVaga(vaga: Vaga):Observable<Vaga[]>{
-    return this._httpClient.post<Vaga[]>(this.url,vaga);
+    return this._httpClient.post<Vaga[]>(this.url+"/cadastrar",vaga);
   }
 
   atualizarVaga(id:any ,vaga: Vaga):Observable<Vaga[]>{
-    const urlAtualizar = `${this.url}/${id}`;
+    const urlAtualizar = `${this.url}/editar/${id}`;
     return this._httpClient.put<Vaga[]>(urlAtualizar,vaga);
   }
 
   removerVaga(id:any):Observable<Vaga[]>{
-    const urlDeletar = `${this.url}/${id}`;
+    const urlDeletar = `${this.url}/vagas/${id}`;
     return this._httpClient.delete<Vaga[]>(urlDeletar);
   }
 }
